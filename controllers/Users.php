f152cbc767774c99d9026f2d9e537f8614a69134
theomhn <theo.menchon@hotmail.fr>
2023-06-01 09:34:04 +0200

    Modification des méthodes afin qu'elles soient génériques.

--- controllers/Users.php
+++ controllers/Users.php
@@ -2,39 +2,35 @@
 
 class Users extends Controller
 {
+    protected $modelName = "User";
 
     public function __construct()
     {
-        $this->loadModel("User");
+        $this->loadModel($this->modelName);
     }
 
-    public function getAll()
+    public function post() // create a new user
     {
-        // Test si j'ai le droit
-        echo json_encode($this->pdo['User']->getAll());
-    }
-    public function get($id)
-    {
-        // Test si j'ai le droit
-        echo json_encode($this->pdo['User']->get($id));
-    }
+        $salt = bin2hex(random_bytes(16));
+        $token = bin2hex(random_bytes(32));
 
-    public function post()
-    {
-        // Test si j'ai le droit
-        echo json_encode($this->pdo['User']->create($_POST));
+        $obj = [
+            'pseudo' => $_POST['pseudo'],
+            'email' => $_POST['email'],
+            'password' => password_hash($_POST['password'] . $salt, PASSWORD_DEFAULT),
+            'salt' => $salt,
+            'token' => $token
+        ];
+        /* var_dump($obj); */
+        $user = $this->pdo['User']->create($obj);
+        echo json_encode($user);
     }
 
     public function put()
     {
-        // Test si j'ai le droit
-        parse_str(file_get_contents('php://input'), $_PUT);
+        /* a tester/debug  */
+        $result = parse_str(file_get_contents('php://input'), $_PUT);
+        var_dump($result);
         echo json_encode($this->pdo['User']->update($_PUT));
-    }
-
-    public function delete($id)
-    {
-        // Test si j'ai le droit
-        echo json_encode($this->pdo['User']->delete($id));
     }
 }
