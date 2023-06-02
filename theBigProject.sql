94e5add3ff18764e24604e7f703adf8fe002d661
theomhn <theo.menchon@hotmail.fr>
2023-06-02 17:12:28 +0200

    new update database

--- theBigProject.sql
+++ theBigProject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : lun. 22 mai 2023 à 15:21
+-- Généré le : ven. 02 juin 2023 à 15:11
 -- Version du serveur : 5.7.39
 -- Version de PHP : 8.2.0
 
@@ -50,6 +50,18 @@ CREATE TABLE `tournaments` (
   `date_end` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `tournaments`
+--
+
+INSERT INTO `tournaments` (`id`, `title`, `game`, `nbParticipants`, `date_start`, `date_end`) VALUES
+(1, 'Tournoi 1vs1 Juin', 'Rocket League', 16, '2023-06-01 00:00:00', '2023-06-30 23:59:00'),
+(2, 'Tournoi 1vs1 Juillet', 'Rocket League', 16, '2023-07-01 00:00:00', '2023-07-31 23:59:00'),
+(3, 'Tournoi 1vs1 Aout', 'Rocket League', 16, '2023-08-01 00:00:00', '2023-08-31 23:59:00'),
+(4, 'Tournoi 1vs1 Septembre', 'Rocket League', 16, '2023-09-01 00:00:00', '2023-09-30 23:59:00'),
+(5, 'Tournoi 1vs1 Octobre', 'Rocket League', 16, '2023-10-01 00:00:00', '2023-10-31 23:59:00'),
+(6, 'Tournoi 1vs1 Novembre', 'Rocket League', 16, '2023-11-01 00:00:00', '2023-11-30 23:59:00');
+
 -- --------------------------------------------------------
 
 --
@@ -66,17 +78,31 @@ CREATE TABLE `users` (
   `active` tinyint(1) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `pseudo`, `email`, `password`, `salt`, `token`, `active`) VALUES
+(1, 'Theo', 'theo.menchon@hotmail.fr', '$2y$10$9SEWX7dICYqvOXE4DNPCOufzCOdvZuyjXI86UJU1koneSfOMEOg1K', '9015d7022ebe8d9606b05cd28de0727a', 'f6d287ce7e8c8b27b228f313a2f8c19d86039c6d5273b20904567ea1a9e353f4', 1);
+
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users_events`
+-- Structure de la table `users_tournaments`
 --
 
-CREATE TABLE `users_events` (
+CREATE TABLE `users_tournaments` (
   `id` int(11) NOT NULL,
-  `users_id` int(11) NOT NULL,
-  `events_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `tournament_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `users_tournaments`
+--
+
+INSERT INTO `users_tournaments` (`id`, `user_id`, `tournament_id`) VALUES
+(2, 1, 1);
 
 --
 -- Index pour les tables déchargées
@@ -101,12 +127,10 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `users_events`
+-- Index pour la table `users_tournaments`
 --
-ALTER TABLE `users_events`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `users_id` (`users_id`),
-  ADD KEY `events_id` (`events_id`);
+ALTER TABLE `users_tournaments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -122,30 +146,19 @@ ALTER TABLE `scores`
 -- AUTO_INCREMENT pour la table `tournaments`
 --
 ALTER TABLE `tournaments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT pour la table `users_events`
+-- AUTO_INCREMENT pour la table `users_tournaments`
 --
-ALTER TABLE `users_events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `users_events`
---
-ALTER TABLE `users_events`
-  ADD CONSTRAINT `users_events_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `users_events_ibfk_2` FOREIGN KEY (`events_id`) REFERENCES `tournaments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `users_tournaments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
