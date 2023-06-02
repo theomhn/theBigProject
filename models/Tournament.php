af519e26ee6028634288d5831da014876dd08911
theomhn <theo.menchon@hotmail.fr>
2023-06-02 17:09:10 +0200

    Création de la requête afin qu'un utilisateur puisse rejoindre un tournoi

--- models/Tournament.php
+++ models/Tournament.php
@@ -7,4 +7,12 @@ class Tournament extends Model
     {
         parent::__construct("tournaments");
     }
+
+    public function join($userId, $tournamentId)
+    {
+        //faire un requête qui récupère le l'utilisateur et une le tournoi !
+
+        //faire insertion en bdd dans la table event
+        $this->_connexion->query("INSERT INTO users_tournaments (user_id,tournament_id) VALUES ($userId, $tournamentId);");
+    }
 }
