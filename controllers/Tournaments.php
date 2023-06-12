695a7665f149d14bd5f4918dc419f85e169d25bd
theomhn <theo.menchon@hotmail.fr>
2023-06-12 10:02:24 +0200

    Implémentation des conditions qui permettent de rejoindre un tournoi

--- controllers/Tournaments.php
+++ controllers/Tournaments.php
@@ -18,21 +18,68 @@ class Tournaments extends Controller
         $this->render("listTournaments", ["styles" => ["tournament"]]);
     }
 
-    public function join($id)
+    public function join($idTournament)
     {
+        $tournament = $this->model->get($idTournament);
+
+        if (!isset($tournament)) {
+            http_response_code(404);
+            echo json_encode("Le tournoi n'existe pas");
+            return false;
+        }
+
+        $date = date("Y-m-d H:i:s");
+        if ($date < $tournament['date_start']) {
+            http_response_code(403);
+            echo json_encode("Le tournoi n'a pas commencé, il commencera le " . $tournament['date_start']);
+            return false;
+        }
+        if ($date > $tournament['date_end']) {
+            http_response_code(403);
+            echo json_encode("Le tournoi est terminé");
+            return false;
+        }
+
+        $allParticipants = $this->model->getParticipants($idTournament); // les participants
+
+        if (count($allParticipants) >= $tournament['nbParticipants']) {
+            http_response_code(403);
+            echo json_encode("Le tournoi est complet");
+            return  false;
+        }
+
         $obj = [
             'user_id' => USER['id'],
-            'tournament_id' => $id
+            'tournament_id' => $idTournament
         ];
 
         $associationModel = $this->loadModel('UserTournament');
-        if ($associationModel->create($obj)) {
+        try {
+            $associationModel->create($obj);
+        } catch (Exception $e) {
             echo json_encode(true);
-            
-        } else {
-            http_response_code(400);
-            echo json_encode(false);
+            return true;
         }
+
+        $games = $this->model->getGames($idTournament); // récupère les games d'un tournoi
+        $lastGame = end($games); // récupère le dernier game créé
+        $gameModel = $this->loadModel('Game');
+
+
+        if (!$lastGame || !empty($lastGame['user1_id']) && !empty($lastGame['user2_id'])) {
+            $obj = [
+                'tournament_id' => $idTournament,
+                'user1_id' => USER['id'],
+                'step' => 0
+            ];
+            $lastGame = $gameModel->create($obj);
+        } else {
+            // faire rejoindre un joueur
+            $obj = ['user2_id' => USER['id']];
+            $gameModel->update($lastGame['id'], $obj);
+        }
+
+        echo json_encode(true);
     }
 
     public function post()
@@ -49,6 +96,4 @@ class Tournaments extends Controller
         $tournament = $this->model->create($obj);
         echo json_encode($tournament);
     }
-
-    
 }
