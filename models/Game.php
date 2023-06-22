d8234060b5a3ba5a521a38d847c64ce115228a67
theomhn <theo.menchon@hotmail.fr>
2023-06-22 16:53:08 +0200

    Création des méthodes getByTournamentId et isDone

--- models/Game.php
+++ models/Game.php
@@ -7,4 +7,21 @@ class Game extends Model
     {
         parent::__construct("matchs");
     }
+
+    public function getByTournamentId($tournamentId)
+    {
+        $games = $this->_connexion->query("SELECT * FROM $this->table WHERE tournament_id = $tournamentId;")->fetch(PDO::FETCH_ASSOC);
+        if ($games === false) {
+            return [];
+        }
+        return $games;
+    }
+
+    //vérifier si le match est déjà validé
+    function isDone($match)
+    {
+        return isset($match['score1_player1']) && isset($match['score2_player1']) && isset($match['score1_player2']) && isset($match['score2_player2'])
+            && $match['score1_player1'] == $match['score2_player1'] && $match['score1_player2'] == $match['score2_player2']
+            && $match['score1_player1'] != $match['score1_player2'];
+    }
 }
